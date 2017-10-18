76ff101d74d8e9a851852fde86b759445dcb90e6
Melvin Walls <mwalls67@gmail.com>
2017-10-17 23:35:48 -0700

    vm/machine: add abosolute/indirect LD for packets

--- src/vm/machine.rs
+++ src/vm/machine.rs
@@ -1,4 +1,10 @@
 #![allow(dead_code)]
+extern crate byteorder;
+
+use std::io::Cursor;
+
+use self::byteorder::{BigEndian, NativeEndian, ReadBytesExt};
+
 use instruction::*;
 
 const SCRATCH_MEM_SLOTS: usize = 16;
@@ -19,6 +25,10 @@ trait Testing {
     fn accumulator(&self) -> u32;
     fn index(&self) -> u32;
     fn memory(&self) -> &[u32];
+    fn set_frame(&mut self, frame: u32);
+    fn set_accumulator(&mut self, acc: u32);
+    fn set_index(&mut self, index: u32);
+    fn set_memory(&mut self, idx: usize, val: u32);
 }
 
 impl Testing for PsuedoMachine {
@@ -36,6 +46,22 @@ impl Testing for PsuedoMachine {
 
     fn memory(&self) -> &[u32] {
         &self.memory
+    }
+
+    fn set_frame(&mut self, frame: u32) {
+        self.frame = frame;
+    }
+
+    fn set_accumulator(&mut self, acc: u32) {
+        self.accumulator = acc;
+    }
+
+    fn set_index(&mut self, index: u32) {
+        self.index = index;
+    }
+
+    fn set_memory(&mut self, idx: usize, val: u32) {
+        self.memory[idx] = val;
     }
 }
 
@@ -59,10 +85,40 @@ impl PsuedoMachine {
         self.memory[n]
     }
 
-    /// Load immediate `k` into the accumulator.
-    fn ldi(&mut self, k: u32) -> Option<u32> {
-        self.accumulator = k;
-        None
+    /// Load into the accumulator.
+    fn ld_u32(&mut self, mode: u16, k: u32, pkt: &[u8]) -> Result<Option<u32>, ()> {
+        match mode {
+            MODE_IMM => {
+                self.accumulator = k;
+                Ok(None)
+            },
+            MODE_ABS => {
+                if k as usize >= pkt.len() {
+                    return Err(());
+                }
+                let mut cur = Cursor::new(&pkt[k as usize..]);
+                let ret = cur.read_u32::<BigEndian>();
+                if ret.is_err() {
+                    return Err(());
+                }
+                self.accumulator = ret.unwrap();
+                Ok(None)
+            },
+            MODE_IND => {
+                let offset: usize = (self.index  + k) as usize;
+                if offset >= pkt.len() {
+                    return Err(());
+                }
+                let mut cur = Cursor::new(&pkt[offset as usize..]);
+                let ret = cur.read_u32::<BigEndian>();
+                if ret.is_err() {
+                    return Err(());
+                }
+                self.accumulator = ret.unwrap();
+                Ok(None)
+            },
+            _ => Err(()),
+        }
     }
 
     /// Execute an instruction.
@@ -70,10 +126,13 @@ impl PsuedoMachine {
     /// Returns Err on bad instruction.
     pub fn execute(&mut self, instr: &Instruction, pkt: &[u8]) -> Result<Option<u32>, ()> {
         let opcode = instr.opcode;
+        let mode = instr.mode();
         let k = instr.k;
         match opcode {
-            CLASS_LD | MODE_IMM | SIZE_W => Ok(self.ldi(k)),
-            _ => Ok(None)
+            CLASS_LD | MODE_IMM | SIZE_W => self.ld_u32(mode, k, pkt),
+            CLASS_LD | MODE_ABS | SIZE_W => self.ld_u32(mode, k, pkt),
+            CLASS_LD | MODE_IND | SIZE_W => self.ld_u32(mode, k, pkt),
+            _ => Err(())
         }
     }
 
@@ -111,6 +170,37 @@ mod tests {
         let pkt = [0 as u8; 64];
         let ret = pm.execute(&instr, &pkt);
         assert!(ret.unwrap() == None);
+        assert!(pm.accumulator() == 0xDEADBEEF);
+    }
+
+    #[test]
+    fn ld() {
+        let mut pm = PsuedoMachine::new();
+        let instr = Instruction::new(CLASS_LD | MODE_ABS | SIZE_W, 0, 0, 3);
+        let mut pkt = [0 as u8; 64];
+        pkt[3] = 0xDE;
+        pkt[4] = 0xAD;
+        pkt[5] = 0xBE;
+        pkt[6] = 0xEF;
+        let ret = pm.execute(&instr, &pkt);
+        assert!(ret.unwrap() == None);
+        println!("{:8X}", pm.accumulator());
+        assert!(pm.accumulator() == 0xDEADBEEF);
+    }
+
+    #[test]
+    fn ld_indirect() {
+        let mut pm = PsuedoMachine::new();
+        pm.set_index(1);
+        let instr = Instruction::new(CLASS_LD | MODE_IND | SIZE_W, 0, 0, 3);
+        let mut pkt = [0 as u8; 64];
+        pkt[4] = 0xDE;
+        pkt[5] = 0xAD;
+        pkt[6] = 0xBE;
+        pkt[7] = 0xEF;
+        let ret = pm.execute(&instr, &pkt);
+        assert!(ret.unwrap() == None);
+        println!("{:8X}", pm.accumulator());
         assert!(pm.accumulator() == 0xDEADBEEF);
     }
 }
